493b65e14147fc39f7d19f8e113e62b9ad9b6b83
Bruno <bruno_moises_stz@hotmail.com>
2023-02-24 15:43:59 -0300

    Inserindo aninhamento de rotas deixando codigo mais limpo

--- src/routes.js
+++ src/routes.js
@@ -2,28 +2,21 @@ import Inicio from './pages/Inicio'
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Favoritos from 'pages/Favoritos'
-import Cabecalho from 'components/Cabecalho'
-import Rodape from 'components/Rodape'
-import Container from 'components/Container'
-import FavoritosProvider from 'contextos/Favoritos'
 import Player from 'pages/Player'
 import NaoEncontrada from 'pages/NaoEncontrada'
+import PaginaBase from 'pages/PaginaBase'
 
 export default function AppRoutes() {
     return (
         <BrowserRouter>
-            <Cabecalho />
-            <Container>
-                <FavoritosProvider>
-                    <Routes>
-                        <Route path='/' element={<Inicio />} />
-                        <Route path='/favoritos' element={<Favoritos />} />
-                        <Route path='/player/:id' element={<Player />} />
-                        <Route path='*' element={<NaoEncontrada />} />
-                    </Routes>
-                </FavoritosProvider>
-            </Container>
-            <Rodape />
+            <Routes>
+                <Route path='/' element={<PaginaBase />}>
+                    <Route index element={<Inicio />} />
+                    <Route path='favoritos' element={<Favoritos />} />
+                    <Route path='player/:id' element={<Player />} />
+                    <Route path='*' element={<NaoEncontrada />} />
+                </Route>
+            </Routes>
         </BrowserRouter>
     )
 }
