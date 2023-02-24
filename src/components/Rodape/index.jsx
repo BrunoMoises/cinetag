eb37b4f83c428580f1a9ab6435f8f790335bf1e0
Bruno <bruno_moises_stz@hotmail.com>
2023-02-24 12:42:07 -0300

    Finalizando header e footer

---
+++ src/components/Rodape/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './Rodape.module.css'
+
+export default function Rodape() {
+  return (
+    <footer className={styles.rodape}>
+        <h2>Desenvolvido por Bruno.</h2>
+    </footer>
+  )
+}
