4bc12a68956b91349e658e9c84443593f050ef73
Bruno <bruno_moises_stz@hotmail.com>
2023-02-24 15:16:48 -0300

    Implementando a funcionalidade de favoritar

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,14 +1,21 @@
 import styles from './Card.module.css'
 import iconeFavoritar from './favoritar.png'
+import iconeDesfavoritar from './desfavoritar.png'
 
 import React from 'react'
+import { useFavoritoContext } from 'contextos/Favoritos'
 
 export default function Card({ id, titulo, capa }) {
+    const { favorito, adiconarFavorito } = useFavoritoContext();
+    const ehFavorito = favorito.some((fav) => fav.id === id);
+    const icone = ehFavorito ? iconeDesfavoritar : iconeFavoritar;
     return (
         <div className={styles.container}>
             <img src={capa} alt={titulo} className={styles.capa} />
             <h2>{titulo}</h2>
-            <img src={iconeFavoritar} alt="Favoritar filme" className={styles.favoritar} />
+            <img src={icone} alt="Favoritar filme" className={styles.favoritar} onClick={() => {
+                adiconarFavorito({ id, titulo, capa })
+            }} />
         </div>
     )
 }
