81a583d59c98c9967b2d1f607188a7256c9a5bc7
Bruno <bruno_moises_stz@hotmail.com>
2023-02-24 16:20:28 -0300

    Mudando a busca dos videos para uma API externa no My Json Server

--- src/pages/Inicio/index.jsx
+++ src/pages/Inicio/index.jsx
@@ -1,11 +1,19 @@
 import Banner from 'components/Banner'
 import Card from 'components/Card'
 import Titulo from 'components/Titulo'
-import React from 'react'
-import videos from 'json/db.json'
+import React, { useEffect, useState } from 'react'
 import styles from './Inicio.module.css'
 
 export default function Inicio() {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetch('https://my-json-server.typicode.com/BrunoMoises/cinetag-api/videos')
+      .then(resposta => resposta.json())
+      .then(dados => {
+        setVideos(dados)
+      })
+  }, [])
   return (
     <>
       <Banner imagem="home" />
