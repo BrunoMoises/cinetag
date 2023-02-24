f8d5c4972f741e8681c1a496451b1b01726d1798
Bruno <bruno_moises_stz@hotmail.com>
2023-02-24 15:34:56 -0300

    Implementando pagina de player ao clicar em um filme

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -4,6 +4,7 @@ import iconeDesfavoritar from './desfavoritar.png'
 
 import React from 'react'
 import { useFavoritoContext } from 'contextos/Favoritos'
+import { Link } from 'react-router-dom'
 
 export default function Card({ id, titulo, capa }) {
     const { favorito, adiconarFavorito } = useFavoritoContext();
@@ -11,8 +12,10 @@ export default function Card({ id, titulo, capa }) {
     const icone = ehFavorito ? iconeDesfavoritar : iconeFavoritar;
     return (
         <div className={styles.container}>
-            <img src={capa} alt={titulo} className={styles.capa} />
-            <h2>{titulo}</h2>
+            <Link className={styles.link} to={`/player/${id}`}>
+                <img src={capa} alt={titulo} className={styles.capa} />
+                <h2>{titulo}</h2>
+            </Link>
             <img src={icone} alt="Favoritar filme" className={styles.favoritar} onClick={() => {
                 adiconarFavorito({ id, titulo, capa })
             }} />
